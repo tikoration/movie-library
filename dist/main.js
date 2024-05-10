ea6c3667a7d45971a283d71a0f755217e4623b9d
elene19 <elenegoginash7@gmail.com>
2024-05-10 17:44:04 +0400

    adding nav markup

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,35 @@
-export function hello() {
-    console.log("please work");
-}
-export function array(inp) {
-    return [inp];
-}
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const route = (event) => {
+    event = event || window.event;
+    event === null || event === void 0 ? void 0 : event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+const routes = {
+    404: "/pages/404.html",
+    "/": "/pages/main.html",
+    "/details": "/pages/details.html",
+};
+const handleLocation = () => __awaiter(void 0, void 0, void 0, function* () {
+    const path = window.location.pathname;
+    console.log(path);
+    const routePath = path || "/";
+    const routeUrl = routes[routePath] || routes[404];
+    const html = yield fetch(routeUrl).then((data) => data.text());
+    const mainPageElement = document.getElementById("main-page");
+    if (mainPageElement) {
+        mainPageElement.innerHTML = html;
+    }
+});
+window.onpopstate = handleLocation;
+window.route = route;
+handleLocation();
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,35 @@
-export function hello() {
-  console.log("please work");
-}
-export function array<T>(inp: T) {
-  return [inp];
-}
+const route = (event: MouseEvent) => {
+  event = event || window.event;
+  event?.preventDefault();
+  window.history.pushState({}, "", (event.target as HTMLAnchorElement).href);
+  handleLocation();
+};
+
+type Route = {
+  [key: string]: string; // Allow any string key
+};
+
+const routes: Route = {
+  404: "/pages/404.html",
+  "/": "/pages/main.html",
+  "/details": "/pages/details.html",
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  console.log(path);
+  const routePath = path || "/";
+  const routeUrl = routes[routePath] || routes[404];
+
+  const html = await fetch(routeUrl).then((data) => data.text());
+
+  const mainPageElement = document.getElementById("main-page");
+  if (mainPageElement) {
+    mainPageElement.innerHTML = html;
+  }
+};
+
+window.onpopstate = handleLocation;
+(window as any).route = route;
+
+handleLocation();
