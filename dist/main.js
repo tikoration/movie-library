591a4e8a3a40d1875ac3651af79198a9642114e3
elene19 <elenegoginash7@gmail.com>
2024-05-10 15:29:10 +0400

    adding babel configuration

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+export function hello() {
+    console.log("please work");
+}
+export function array(inp) {
+    return [inp];
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+export function hello() {
+  console.log("please work");
+}
+export function array<T>(inp: T) {
+  return [inp];
+}
