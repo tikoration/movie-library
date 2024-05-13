ba89129ebcf3e4bb5b0b7b35b9caca6672e409f0
elene19 <elenegoginash7@gmail.com>
2024-05-13 10:30:40 +0400

    making changes to main.ts file

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,3 +1,4 @@
+import Explore from "./components/explore.js";
 import DetailsOfMovie from "./details.js";
 import { getMovieById, getPopularMovies } from "./services/apiMovies.js";
 const mainPageElement = document.getElementById("main-page") as HTMLElement;
@@ -5,7 +6,6 @@ const mainPageElement = document.getElementById("main-page") as HTMLElement;
 const route = (event: MouseEvent) => {
   event = event || window.event;
   event?.preventDefault();
-  // console.log(event.target);
   window.history.pushState({}, "dd", (event.target as HTMLAnchorElement).href);
   handleLocation();
 };
@@ -22,7 +22,6 @@ const routes: Route = {
 
 const handleLocation = async () => {
   const path = window.location.pathname;
-  console.log(path);
 
   const routeUrl = routes[path] || routes[404];
 
@@ -34,10 +33,16 @@ const handleLocation = async () => {
 
   const homeContainer = document.querySelector(".home-container");
   const detailsContainer = document.querySelector(".details-container");
+  const explore = document.getElementById("explore");
+
+  if (explore) {
+    explore.innerHTML = Explore();
+  }
   if (homeContainer) {
     const pop = document.querySelector(".populars");
     const newDiv = document.createElement("div");
-    pop?.append;
+    const data = await getPopularMovies();
+    console.log(data);
   }
   if (detailsContainer) {
     detailsContainer.innerHTML = DetailsOfMovie();
@@ -47,18 +52,3 @@ const handleLocation = async () => {
 window.onpopstate = handleLocation;
 (window as any).route = route;
 handleLocation();
-
-// //////
-
-// In main.ts
-//653346
-// async function getmovie() {
-//   const fetched = await fetch(
-//     `https://api.themoviedb.org/3/movie/popular?api_key=88f63d75ae40120899216aa75faa6c13`
-//   );
-//   console.log(fetched);
-//   const data = await fetched.json();
-//   console.log(data);
-// }
-
-// getmovie();
