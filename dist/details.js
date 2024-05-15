35d941115fd2367f312416060a22f50ed4ff208b
Tinatin <tinatinmghebrishvili@gmail.com>
2024-05-15 14:49:56 +0400

    Revert "restructuring files"
    This reverts commit 6dcf82fc2dbba1533d8e9f3032db79da0c1e448a.

---
+++ dist/details.js
@@ -0,0 +1,52 @@
+export default function DetailsOfMovie() {
+    return `
+  <div class="trailer-description">
+    <div class="trailer">
+      <p class="movie-name">movie name</p>
+      <video src="" class="movie-trailer"></video>
+    </div>
+    <div class="description">
+      <div>
+        <div>
+          <img class="details-movie-img" />
+        </div>
+        <div>
+          <div>
+            <p>Type :</p>
+            <span class="type"></span>
+          </div>
+          <div>
+            <p>Status :</p>
+            <span class="status"> wfeerwerw</span>
+          </div>
+          <div>
+            <p>Studios :</p>
+            <span class="studios"></span>
+          </div>
+          <div>
+            <p>Duration :</p>
+            <span class="duration"></span>
+          </div>
+          <div>
+            <p>Genres :</p>
+            <span class="genres"></span>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <p>description :</p>
+        <span class="desc">
+  
+        </span>
+      </div>
+    </div>
+  </div>
+  <div class="details-popular">
+    <p>Popular Movies</p>
+    <div class="details-popular-cont">
+   
+    </div>
+  </div>
+  `;
+}
